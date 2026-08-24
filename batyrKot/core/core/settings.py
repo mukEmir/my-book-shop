640d32ffc1c8c26df1e404edf78335c7af851dda
Malignant <t10198108@gmail.com>
2026-08-24 17:20:26 +0600

    Update settings.py

--- batyrKot/core/core/settings.py
+++ batyrKot/core/core/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Добавить сюда!
 ]
 
 # ------------------------------------------------------------

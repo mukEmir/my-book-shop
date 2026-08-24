3af7ba520ce1fa77d0831ec15003c9c8facf772e
Malignant <t10198108@gmail.com>
2026-08-24 16:54:51 +0600

    Update settings.py

--- batyrKot/core/core/settings.py
+++ batyrKot/core/core/settings.py
@@ -141,3 +141,5 @@ STRIPE_SECRET_KEY = 'sk_test_...'
 # По умолчанию
 # ------------------------------------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
